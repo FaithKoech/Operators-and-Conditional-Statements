f3ec2e05977c8f3788250ed64f9a7bff86ab1b5f
FaithKoech <faithkoech1@gmail.com>
2019-11-21 14:06:18 +0300

    Operators&Conditional Statements

---
+++ Ifelse.py
@@ -0,0 +1,24 @@
+""""
+if...statement in python syntax
+if test expression:
+ //statements
+"""
+number = 100
+# if
+# if number > 150:
+#     print("I am ok")
+
+# # if else
+# if number > 150:
+#   print("I am ok")
+# else:
+#         print("I am not ok")
+
+# if...elif
+num = 20
+if num > 0:
+  print("{} is > 0".format(num))
+elif num ==10:
+    print("{} is ==10".format(num))
+else:
+    print("{} is a negative number". format(num))
---
+++ Operators.py
@@ -0,0 +1,60 @@
+# Arithmetics /  %  //  **-exponent
+# x=10
+# y=5
+# print(x+y)
+# print(x-y)
+# print(x/y)
+# print(x%y)
+# print(x//y)
+# print(x**y)
+
+
+# x=input("Enter a number:")
+# y=input("Enter a number:")
+# sum=int(x)+int(y)
+# print(sum)
+
+# Comparison Operators e.g < > == != >=  <=
+# e.g
+# a=110
+# b=110
+# print("a>b is", a>b)
+# print("a<b is", a<b)
+# print("a==b is", a==b)
+# print("a!=b is", a!=b)
+# print("a>=b is", a>=b)
+# print("a<=b is", a<=b)
+
+
+# Logical Operators i.e and or not
+# 1. AND returns true ONLY if both operands are true
+# 2. OR - Returns TRUE if any of the operands is TRUE
+# 3. NOT - Invert the True value
+
+
+# m = True
+# n = False
+# print("m n is", m and n)
+# print("m or is", m or n)
+# print("not m is", not m)
+
+""""Assiignment Operators - Used to assign value to a variable
+=
++=
+-=
+"""
+# c = 10
+# c += 10 #c=c+10
+# print(c)
+# c-= 5 # c=c-5
+# print(c)
+
+# x=input("Enter a number:")
+# y=input("Enter a number:")
+# sum=int(x)+int(y)
+# print(sum)
+
+# x = input("Enter a number:")
+# y = input("Enter a number:")
+# y -=5
+# print(y)
